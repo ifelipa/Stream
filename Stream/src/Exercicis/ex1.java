d4966765d1ca7f43633a74836d78eef21bdb2a00
isma <ismael.felipa@gmail.com>
2015-11-02 13:17:43 +0100

    +avance ex1

--- Stream/src/Exercicis/ex1.java
+++ Stream/src/Exercicis/ex1.java
@@ -1,40 +1,75 @@
 package Exercicis;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ex1 {
-	public static void main(String[] args) throws IOException{
-
-		FileInputStream in1 = null;
-		FileInputStream in2 = null;
-		FileInputStream in3 = null;
-		FileOutputStream out = null;
+	public static void main(String[] args) {
+		System.out.println("Introduce el numero de ficheros");
+		Scanner sc = new Scanner(System.in);
+		int nfile= sc.nextInt();
 		
-		try {
-			in1 = new FileInputStream("exemple1.txt");
-			in2 = new FileInputStream("exemple1.txt");
-			in3 = new FileInputStream("exemple1.txt");
-			
-			out = new FileOutputStream("union.txt");
-			int c;
-
-			while ((c = in.read()) != -1) {
-				out.write(c);
+		ArrayList<File> fileIn = new ArrayList<File>();
+		
+		for (int i = 0; i < nfile; i++) {
+			System.out.println("Introduce nombre del fichero nº :"+ (i+1));
+			//Creamos el fichero para comprobar si existe
+			sc= new Scanner(System.in);
+			File file = new File(sc.next());
+			if (!file.exists()){
+				System.out.println("el fichero no existe");
+				break;
 			}
-		} finally {
-			if (in != null) {
-				in.close();
-			}
-			if (out != null) {
-				out.close();
+			fileIn.add(file);
+		}
+		
+		//pidiendo el nombre de fichero de salida
+		System.out.println("Introduce el nombre de fichero de salida");
+		sc= new Scanner(System.in);
+		File fileOut = new File(sc.next());
+		
+		for (int i = 0; i < fileIn.size(); i++) {
+			File in = fileIn.get(i);
+			try {
+				copiar( in, fileOut);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
+	}
 
+	private static void copiar(File origen, File destino)throws IOException {
+		FileReader in = null;
+		FileWriter out = null;
+		BufferedReader bfReader= null;
+		
+		try {
+			in = new FileReader(origen);
+			out = new FileWriter(destino, true);
+			bfReader = new BufferedReader(in);
+			String line;
+			while ((line= bfReader.readLine()) != null) {
+				out.write(line);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			in.close();
+			out.close();
+			bfReader.close();
+		}
+		
 	}
 	
+
 	
 
 }
