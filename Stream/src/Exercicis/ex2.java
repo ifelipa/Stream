f69f2965fdc0a3113692e07f1777d595d3bec66f
isma <ismael.felipa@gmail.com>
2015-11-02 16:14:32 +0100

    +avances ex2

---
+++ Stream/src/Exercicis/ex2.java
@@ -0,0 +1,21 @@
+package Exercicis;
+
+import java.io.File;
+
+public class ex2 {
+	public static void main(String[] args) {
+		
+		//dividir
+		String pathname ="bigfile";
+		String baseName="baseName";
+		File bigfile = new File(pathname);
+		int chunkSize = 0;
+		
+		copiarXbytes(pathname, baseName, chunkSize);
+	}
+
+	private static void copiarXbytes(String pathname, String baseName, int chunkSize) {
+	// TODO Auto-generated method stub
+		
+	}
+}
