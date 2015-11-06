ef2adc8c2d817cc0366d1bd5d77d5a7b572df820
isma <ismael.felipa@gmail.com>
2015-11-06 17:06:39 +0100

    +avances

--- Stream/src/ExercicisStream/ex4.java
+++ Stream/src/ExercicisStream/ex4.java
@@ -1,15 +1,42 @@
 package ExercicisStream;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ex4 {
 
 	public static void main(String[] args) {
-		String path="";
+		String path="C:/Users/isma/git/Stream/Stream";
 		copiadeSeguridad(path);
 	}
 
 	private static void copiadeSeguridad(String path) {
-		// TODO Auto-generated method stub
-		
+		File pathDirectory = new File(path);
+		File [] listaFicheros=null;
+		String nBackUp;
+		if (pathDirectory.isDirectory()){
+			listaFicheros= pathDirectory.listFiles();
+			nBackUp=numeroBackUp(listaFicheros);
+			
+		}		
 	}
+
+	private static String numeroBackUp(File[] listaFicheros) {
+		ArrayList<String> dir = new ArrayList<String>();
+		for (File file : listaFicheros) {
+			String aux = file.getName();
+			if (file.isDirectory() && aux.matches("^backUp[0-9]")){
+				System.out.println(file.getName());
+				dir.add(file.getName());
+				
+			}
+		}
+		Collections.sort(dir);
+		
+		System.out.println(dir.size()-1);
+		return null;
+	}
+	
 
 }
