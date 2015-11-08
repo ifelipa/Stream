2ddf8e7344ecbd27782ece8589a42bdb650236f8
isma <ismael.felipa@gmail.com>
2015-11-08 11:10:35 +0100

    +ex4

--- Stream/src/ExercicisStream/ex4.java
+++ Stream/src/ExercicisStream/ex4.java
@@ -4,75 +4,84 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class ex4 {
 
 	public static void main(String[] args) {
-		//String path="C:/Users/isma/git/Stream/Stream";
-		String path="/home/users/inf/wiaw2/iaw188216/git/Stream/Stream/";
-		
+		// String path="C:/Users/isma/git/Stream/Stream";
+		String path = "/home/users/inf/wiaw2/iaw188216/git/Stream/Stream/";
+
 		File pathDirectory = new File(path);
-		File [] listFileDirectory=null;
-		
-		if (pathDirectory.isDirectory()){
-			listFileDirectory= pathDirectory.listFiles();
-			
-			String directoryNameNewPath = path+"/backUp"+numeroBackUp(listFileDirectory);			
+		File[] listFileDirectory = null;
+
+		if (pathDirectory.isDirectory()) {
+			listFileDirectory = pathDirectory.listFiles();
+
+			String directoryNameNewPath = path + "/backUp" + numeroBackUp(listFileDirectory);
 			File newDictory = new File(directoryNameNewPath);
 			newDictory.mkdir();
-			copyBackUp(listFileDirectory,directoryNameNewPath);			
-		}
-	}
-
-	private static void copyBackUp(File[] listFileDirectory, String directoryNameNewPath) {
-		for (File file : listFileDirectory) {
-			if (file.isFile()){
-				if (file.canRead()){
-					
-				
-				FileInputStream in = null;
-				FileOutputStream out = null;
-				
-				try {
-					in = new FileInputStream(file);
-					
-					
-				} catch (FileNotFoundException e) {
-
-					e.printStackTrace();
-				}
-				
-				
-				
-				}else{
-					System.out.println("No se puede leer el fichero" + file.getName());				}
-				
+			try {
+				createBackUp(listFileDirectory, directoryNameNewPath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
-		
 	}
 
+	private static void createBackUp(File[] listFileDirectory, String directoryNameNewPath) throws IOException {
+		for (File inFile : listFileDirectory) {
+			if (inFile.isFile()) {
+				if (inFile.canRead()) {
+					File outFile = new File(directoryNameNewPath + "/" + inFile.getName());
+					outFile.createNewFile();
+					createCopy(inFile, outFile);
+				} else {
+					System.out.println("No se puede leer el fichero" + inFile.getName());
+				}
+			}
+		}
+	}
 
+	private static void createCopy(File inFile, File outFile) throws IOException {
+		InputStream in = null;
+		OutputStream out = null;
 
+		try {
+			in = new FileInputStream(inFile);
+			out = new FileOutputStream(outFile);
+			int b = 0;
 
+			while ((b = in.read()) != -1) {
+				out.write(b);
+			}
+		
+		} finally {
+			in.close();
+			out.close();
+		}
+
+	}
 
 	private static int numeroBackUp(File[] listaFicheros) {
-		
-		int numberBackUp=-1;
+
+		int numberBackUp = -1;
 		for (File file : listaFicheros) {
-			String aux = file.getName();			
-			if (file.isDirectory() && aux.matches("^backUp[0-9]")){
+			String aux = file.getName();
+			if (file.isDirectory() && aux.matches("^backUp[0-9]")) {
 				System.out.println(file.getName());
-				int n = Integer.parseInt(aux.substring(aux.length()-1, aux.length()));
-				if(n>numberBackUp){
-					numberBackUp=n;
+				int n = Integer.parseInt(aux.substring(aux.length() - 1, aux.length()));
+				if (n > numberBackUp) {
+					numberBackUp = n;
 				}
 			}
 		}
-		return (numberBackUp+1);
+		return (numberBackUp + 1);
 	}
-	
 
 }
