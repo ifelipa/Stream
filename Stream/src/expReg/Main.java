a43b627f2677559e1af38d40e4b0af7f32f87cab
iaw188216 <iaw188216@h11.informatica.escoladeltreball.org>
2015-11-11 17:27:15 +0100

    +más

---
+++ Stream/src/expReg/Main.java
@@ -0,0 +1,49 @@
+package expReg;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * las cadenas (string) nunca debe usarse un comparado logico para
+		 * compararlos he de usarse el equals(Object anObject), compareTo(String
+		 * anotherString)
+		 * 
+		 * 
+		 * [a-d[m-p]] Un caràcter entre a i d o entre m i p : [a-dm-p]
+		 * [[a-d][m-p][x-z]]
+		 */
+
+		String text = "El que pregunta una vegada és maldestra una vegada, el que no pregunta mai sempre és tonto";
+
+		String regex = "[a-z]{7}[aeiou]";
+
+		Pattern p = Pattern.compile(regex);
+
+		Matcher m = p.matcher(text);
+
+		while (m.find() == true) {
+			System.out.println("Cadena: " + m.group());
+		}
+		
+		
+		 String line = "This order was placed for QT3000! OK?";
+	      String pattern = "(.*)(\\d+)(.*)";
+
+	      // Create a Pattern object
+	      Pattern r = Pattern.compile(pattern);
+
+	      // Now create matcher object.
+	      Matcher m1 = r.matcher(line);
+	      while(m1.find( )) {
+	         System.out.println("Found value: " + m1.group(1));
+	         System.out.println("Found value: " + m1.group(2));
+	         System.out.println("Found value: " + m1.group(3));
+	         System.out.println("Found value: " + m1.group(4));
+	      }
+
+	}
+
+}
